4142e41b1d96aa3d036e5ceb57c5feb61feb1229
RajshriSkillup <rajashreep@skillup.online>
2025-07-29 06:02:03 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login'
 import Sign_Up from './Components/Sign_Up/Sign_Up';
+import AppointmentFormIC from './Components/InstantConsultation/AppointmentFormIC/AppointmentFormIC';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearchIC from './Components/InstantConsultation/FindDoctorSearchIC/FindDoctorSearchIC';
 // Function component for the main App
 function App() {
 
@@ -26,6 +29,8 @@ function App() {
             <Route path="/" element={<Landing_Page/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
+            <Route path='/instant-consultation' element={<InstantConsultation/>} />
+            <Route path="/finddoctor" element={<FindDoctorSearchIC />} />
             </Routes>
           
         </BrowserRouter>
